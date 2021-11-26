d8282d46e0fbf61cf25811ccae9d6caf3acba28c
yanlax <72750776+yanlax@users.noreply.github.com>
2021-11-26 20:49:57 +0500

    Add files via upload

---
+++ MyDeposit/MyDeposit/MyDeposit.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    double sumDeposit = 0;
+    double percentBet = 0;
+    double accruedPercent = 0;
+
+    while (true)
+    {
+        cout << "\nВведите сумму вклада: "; cin >> sumDeposit;
+
+        if (sumDeposit > 0)
+        {
+            cout << "\nВведите проценьную ставку: ";  cin >> percentBet;
+
+            if ((percentBet > 0) && (percentBet < 50))
+            {
+                accruedPercent = sumDeposit * percentBet / 100;
+
+                cout << "\nДанные по вкладу:"
+                    << "\nСумма вклада: " << sumDeposit
+                    << "\nПроцентная ставка: " << percentBet
+                    << "\nНачисленные проценты: " << accruedPercent
+                    << "\nОбщая сумма: " << sumDeposit + accruedPercent << endl;
+                break;
+            }
+            else
+            {
+                cout << "\nПроцентная ставка не может быть меньше 0 или больше 50! Повторите попытку!" << endl;
+                continue;
+            }
+        }
+        else
+        { 
+            cout << "\nВклад не может быть отрицательным! Повторите попытку!" << endl;
+            continue;
+        }
+    }
+    system("pause");    
+}
+
