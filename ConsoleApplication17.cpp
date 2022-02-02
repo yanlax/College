661da75d5cf1dd3c1b0c441a9e32c50522063a3f
yanlax <72750776+yanlax@users.noreply.github.com>
2022-02-02 10:52:41 +0500

    Add files via upload

---
+++ ConsoleApplication17.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int length = 0;
+    int width = 0;
+    int number = 0;
+    int finalLenght = 0;
+    int finalWidth = 0;
+    int i = 0;
+
+    cout << "Введите длину X: ";
+    cin >> length;
+    cout << "Введите ширину Y: ";
+    cin >> width;
+
+    for (;;)
+    {
+        if ((length > 1) && (width > 1))
+        {
+            number = length / width;
+            finalLenght = width;
+            finalWidth = length - (width * number);
+            width = finalWidth;
+            length = finalLenght;
+            i = i + number;
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << "Кол-во получившихся квадратов: " << i;
+    cout << "\nСторона последнего квадрата: " << length << endl;
+
+    system("pause");
+}
