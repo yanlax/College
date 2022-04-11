d3e4652889fdb7fb63ce2805993db9592e7c58c0
yanlax <72750776+yanlax@users.noreply.github.com>
2022-04-11 08:06:26 +0300

    Update ConsoleApplication12.cpp

--- HomeWork/ConsoleApplication12.cpp
+++ HomeWork/ConsoleApplication12.cpp
@@ -1,16 +1,39 @@
-﻿#include <iostream>
+#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 void printArray(int array[], int size)
-{	
+{
 	cout << "\nВ массиве содержатся следующие элементы:\n" << endl;
 	for (int i = 0; i < size; i++)
 	{
 		cout << i << ": " << array[i] << endl;
 	}
 }
+
+void printNewArray(int array[], int size)
+{
+	cout << "\nВ новом массиве содержатся следующие элементы:\n" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cout << i << ": " << array[i] << endl;
+	}
+}
+
 void minAndMax(int array[], int size)
 {
+	int max = array[0];
+	int min = array[0];
+
+	for (int i = 1; i < 10; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+		else if (array[i] < min)
+			min = array[i];
+	}
+	array[0] = max;
+	array[size] = min;
 
 }
 
@@ -37,7 +60,24 @@ int main()
 		}
 	}
 	printArray(massive, size);
+	//minAndMax(massive, size);
+	/*int min = 0, max = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (massive[i] < massive[min])
+			min = i;
+
+		if (massive[i] > massive[max])
+			max = i;
+	}
+
+	int tmp = massive[max];
+	massive[max] = massive[min];
+	massive[min] = tmp;*/
+	sort(massive, massive + size, greater<int>());
+
+	printNewArray(massive, size);
 
 	system("pause");
 }
-
